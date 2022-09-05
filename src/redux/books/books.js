eff3e8b20a168fb09053c0fa5fb3b6173be9e46d
Collins <mussocollins@gmail.com>
2022-09-05 15:23:45 +0300

    Initial changes

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -89,21 +89,4 @@ const bookReducer = (state = bookArray, action) => {
   }
 };
 
-// const bookReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case ADD_BOOK:
-//       return [...state, action.payload];
-
-//     case REMOVE_BOOK:
-//       return state.filter((book) => book.id !== action.payload);
-//     case GET_BOOK:
-//       return [
-//         ...action.payload,
-//       ];
-
-//     default:
-//       return state;
-//   }
-// };
-
 export default bookReducer;
